9673559e073506bc078c43bd23d5e02705237653
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-25 06:09:12 +0300

    fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 14:16:54 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/03/24 22:32:11 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/03/25 06:10:29 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,33 +31,31 @@ static char	*buffer_read(int fd)
 	return (buffer);
 }
 
-void    *ft_memcpy(void *dest, const void *src, size_t size)
+void	*ft_memcpy(void *dest, const void *src, size_t size)
 {
-        unsigned char           *result;
-        const unsigned char     *source;
-        size_t                          i;
+	unsigned char		*result;
+	const unsigned char	*source;
+	size_t				i;
 
-        result = dest;
-        source = src;
-        i = 0;
-        if (dest == src || !size)
-                return (dest);
-        while (i < size)
-        {
-                result[i] = source[i];
-                i++;
-        }
-        return (dest);
+	result = dest;
+	source = src;
+	i = 0;
+	if (dest == src || !size)
+		return (dest);
+	while (i < size)
+	{
+		result[i] = source[i];
+		i++;
+	}
+	return (dest);
 }
 
 static char	*get_stash(char *buffer)
 {
 	char	*str;
-	int	i;
 	char	*return_val;
 
 	str = ft_strchr(buffer, '\n') + 1;
-	i = 0;
 	return_val = ft_calloc(ft_strlen(str, '\0') + 1, sizeof(char));
 	if (!return_val)
 		return (NULL);
@@ -67,21 +65,29 @@ static char	*get_stash(char *buffer)
 
 char	*get_next_line(int fd)
 {
-	static char	*stash = "";
+	static char	*stash = NULL;
 	char		*buffer;
 	char		*line;
 
 	while (1 == 1)
 	{
 		buffer = buffer_read(fd);
-		if (!buffer)
+		if (!stash && buffer)
+			stash = "";
+		else if (ft_strchr(buffer, '\n') || (ft_strchr(stash, '\n') && !buffer))
+			break ;
+		else if (!buffer)
 			return (NULL);
-		if (ft_strchr(buffer, '\n'))
-			break;
 		stash = ft_strjoin(stash, buffer, '\0');
 	}
-	line = ft_strjoin(stash, buffer, '\n');
-	stash = NULL;
+	if (ft_strchr(stash, '\n') && !buffer)
+	{
+		line = ft_strjoin("", stash, '\n');
+		buffer = stash;
+	}
+	else
+		line = ft_strjoin(stash, buffer, '\n');
+	// free(stash);
 	stash = get_stash(buffer);
 	free(buffer);
 	return (line);
